2ad690a3b33cb3ae83bbc99651e0c9cd8545d988
theetje <delange.thomas@gmail.com>
2017-11-18 16:57:57 +0100

    Initial Commit

---
+++ PersonalityQuizTests/PersonalityQuizTests.swift
@@ -0,0 +1,36 @@
+//
+//  PersonalityQuizTests.swift
+//  PersonalityQuizTests
+//
+//  Created by Thomas De lange on 18-11-17.
+//  Copyright © 2017 Thomas De lange. All rights reserved.
+//
+
+import XCTest
+@testable import PersonalityQuiz
+
+class PersonalityQuizTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
